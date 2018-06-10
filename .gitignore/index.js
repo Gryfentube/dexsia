65486bbb4eaa7f179a3ee649d77d110008619c76
Gryfentube <gryfentube@gmail.com>
2018-06-10 23:46:22 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -11,10 +11,13 @@ bot.on('ready', () => {
     bot.channels.get('455070342612910081').send("Je suis en ligne ! :D");
 });
 
-bot.login(process.env.TOKEN);
-bot.on("guildMemberAdd", member => {
-    var dC = member.guild.channels.find("name", "welcome");
-    dC.sendMessage('Passe un bon moment dans la **DexSia** ' + member.user + ' 😉');
+bot.login(process.env.TOKEN); //définition du token
+
+bot.on("guildMemberAdd", member => { //Quand un membre entre dans le serveur
+    var dC = member.guild.channels.find("name", "welcome"); //variable pour le salon welcome
+    dC.sendMessage('Passe un bon moment dans la **DexSia** ' + member.user + ' 😉')
+        .then(bot.channels.get('455070342612910081').sendMessage(member.user + "est arrivé dans la DexSia, le message c'est bien affiché))
+        .catch(bot.channels.get('455070342612910081').sendMessage(member.user + "est arrivé dans la DexSia, le message ne s'est pas affiché));
 });
 bot.on("guildMemberRemove", member => {
     var gB = member.guild.channels.find("name", "aurevoir");
