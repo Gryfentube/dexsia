b8f937b193ac150037124b61c50d7aa3c9249c31
Gryfentube <gryfentube@gmail.com>
2018-06-10 22:27:01 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -15,6 +15,9 @@ bot.login(process.env.TOKEN);
 bot.on("guildMemberAdd", member => {
     bot.channels.get('454987596960104452').sendMessage('Passe un bon moment dans la **DexSia** ' + member.user + ' 😉')
 });
+bot.on("guildMemberRemove", member => {
+    bot.channels.get('4549876442928250902').sendMessage('**' + member.user + '** est partie. Rest in pepperoni...')
+});
 bot.on('message', message => {
 
     if (!message.content.startsWith(prefix)) return;
