e613c8ae63988e1141c73f8235084a0ef0974ee8
Gryfentube <gryfentube@gmail.com>
2018-06-12 02:34:11 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -65,21 +65,4 @@ bot.on('message', message => {
     bot.user.setPresence({ game: { name: value}})
         .then(bot.channels.get(consauleDXS).sendMessage({embed: {color: 0x202020, author: {name: "Je joue maintenant à " + value + " grâce à " + author,
       icon_url: "https://cdn.discordapp.com/icons/441664261454823444/1cced0ad87913d0d5232dce11bedb70f.png"}}}))};
-    
-    if (!message.content.startsWith(prefix)) return;
-    var args = message.content.substring(prefix.length).split(" ");
-    switch (args[0].toLowerCase()){
-
-        case "oui":
-            message.channel.send({embed: {color: 3447003, author: {name: author, icon_url: message.member.avatarURL},
-                                title: "Titre numéro 1", url: "https://www.google.fr/search?q=ON+CHERCHE+QUOI+LES+GARS+%3F&rlz=1C1CHBD_frFR725FR725&oq=ON+CHERCHE+QUOI+LES+GARS+%3F&aqs=chrome..69i57.6119j1j7&sourceid=chrome&ie=UTF-8",
-                                description: "Ouais donc les gars on fait des test pour la suite",
-                                fields: [{name: "C'EST LA SUITE", value: "EN FAIT UAI"},
-                                {name: "UESH LES POTES",
-                                 value: "REGARDE CE QU'ON PEUT FAIRE [C'EST FOU](https://www.google.fr/search?q=ON+CHERCHE+QUOI+LES+GARS+%3F&rlz=1C1CHBD_frFR725FR725&oq=ON+CHERCHE+QUOI+LES+GARS+%3F&aqs=chrome..69i57.6119j1j7&sourceid=chrome&ie=UTF-8) FJKGDKFGJBD"},
-                                {name: "Et en plus les trucs fonctionnes", value: "vouala *alouwa* **__alfksds__** de danana to."}],
-                                timestamp: new Date(), footer: {icon_url: message.member.avatarURL, text: "© Gryf 2049"}}});
- break;
-    }
-
 })
