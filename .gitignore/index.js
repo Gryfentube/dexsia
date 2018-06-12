9a9b379b99642ac05fc01929cd8885187e52c7cd
Gryfentube <gryfentube@gmail.com>
2018-06-12 02:51:49 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -44,10 +44,10 @@ bot.on('message', message => {
     var value = message.content;
     if (message.channel.id === activitDXS) {
         bot.user.setPresence({ game: { name: value}})
-            .then(bot.channels.get(consauleDXS).sendMessage({embed: {color: 0x202020, author: {name: "Je joue maintenant à " + value + " grâce à " + author,
+            .then(bot.channels.get(consauleDXS).sendMessage({embed: {color: 0x3ac400, author: {name: "Je joue maintenant à " + value + " grâce à " + author,
                                                                icon_url: "https://cdn.discordapp.com/icons/441664261454823444/1cced0ad87913d0d5232dce11bedb70f.png"}}}))};
     if (message.channel.id === annPoDXS){
-        bot.channels.get(annDXS).sendMessage({embed: {color: 0xe43281, fields: [{name: "ANNONCE", value: value}]}}) //annonce
+        bot.channels.get(annDXS).sendMessage({embed: {color: 0x3ac400, fields: [{name: "ANNONCE", value: value}]}}) //annonce
             .then(bot.channels.get(consauleDXS).sendMessage({embed: {color: 0xe43281, author: {name: "Nouvelle annonce envoyé par " + author,
                                                                icon_url: "https://cdn.discordapp.com/icons/441664261454823444/1cced0ad87913d0d5232dce11bedb70f.png"},
                                                                fields: [{name: "L'annonce est :", value: value}]}}))}; //console
