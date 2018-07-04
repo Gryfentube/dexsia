3502b0cd5d589d4c66a2002b0500cf31037949f9
Gryfentube <gryfentube@gmail.com>
2018-07-04 20:10:12 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -83,7 +83,7 @@ bot.on('message', message => {
     if (message.content.startsWith("Hola")) {
     message.channel.sendMessage("Hoola " + author + " ! Como estas ? <:051happy1:458741130708779028>");
     };
-    if ((message.content.includes("mdr")) || (message.content.includes("MDR")) {
+    if ((message.content.includes("mdr")) || (message.content.includes("MDR"))) {
         if (author = "DexSia") {
     message.channel.sendMessage("Ce n'est pas bien");
         }
