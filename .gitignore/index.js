ed5c13f68083f05ef2d386dab1b4b00429d6c12c
Bjrjesuisjack <40117726+Bjrjesuisjack@users.noreply.github.com>
2018-07-08 20:57:42 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -239,13 +239,13 @@ bot.on('message', message => {
         if (message.content.includes("Phrase à changer")) {
             message.channel.sendMessage("La réponse");
         }
-        if (message.content.includes("CECI DOIT ÊTRE CHANGÉ")) {
+        if (message.content.includes("a meilleure arme")) {
             rando2();
             if (randnum == 1) {
-                message.channel.sendMessage("LE BOT REPOND ICI");
+                message.channel.sendMessage("Eh le bizon c'est vraiment la meilleure arme du jeu <:051cool:458741113038176267>");
             }
             if (randnum == 0) {
-                message.channel.sendMessage("LE BOT REPOND ÇA UNE FOIS SUR DEUX");
+                message.channel.sendMessage("Eh l'Ak c'est vraiment la meilleure arme du jeu <:051cool:458741113038176267>");
             }
         
         }
