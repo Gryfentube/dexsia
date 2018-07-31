1ea614d32b4c59cbb3528bf2cb8912b3b455a20d
Gryfentube <gryfentube@gmail.com>
2018-07-31 12:44:46 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -261,8 +261,8 @@ bot.on('message', message => {
             }
         }
         if (messlow.includes("emoj")) {
-            var emojis = bot.emojis.split("`:/n`");
-            var res = emojis.array();
+            var emojis = bot.emojis.array();
+           // var res = emojis.split("`:/n`");
             message.channel.sendMessage("`" + emojis + "`");
         }
         if (messlow.includes("PHRASE EN MINUSCULE")) {
