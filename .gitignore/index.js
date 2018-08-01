b642776587409906cffeb2b0d0e217bda2922f3e
Gryfentube <gryfentube@gmail.com>
2018-08-01 17:28:44 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,14 +1,17 @@
 //constante
-	const Discord = require('discord.js');
-	const low = require('lowdb');
-	const faker = require('faker');
-	const FileSync = require('lowdb/adapters/FileSync')
-	const db = low(adapter);
+const Discord = require('discord.js');
+const low = require('lowdb');
+const jimp = require('jimp')
+const faker = require('faker');
+faker.locale = "fr";
+const FileSync = require('lowdb/adapters/FileSync')
+const bot = new Discord.Client();
+const adapter = new FileSync('database.json');
+var i = 0;
+const db = low(adapter);
     db.defaults({ ann:[]})
         .write()
-	const bot = new Discord.Client();
-	const adapter = new FileSync('database.json');
-	bot.login(process.env.TOKEN);
+bot.login(process.env.TOKEN);
 		//salon 
 			//salons DexSia Introduce YourSelf
 			    const annDXSIY = "452800422655033365"; //salon annonce DexSia Introduce Yourself
